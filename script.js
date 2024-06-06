71b5a6726a3138d72c8f5f358ae64de6281f97c2
seftel72 <bseftel@gmail.com>
2024-06-06 13:52:51 -0400

    Second commit for guess game

--- script.js
+++ script.js
@@ -6,11 +6,74 @@
 // document.querySelector('.score').textContent = 11;
 // document.querySelector('.guess').value = 6;
 
+// declare variables
+let secretNumber = Number(1);
+let guess = Number(1);
+let score = Number(20);
+
+//function declarations
+function getRandomInt(min, max) {
+  console.log('Random Num Gen Call');
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function isCorrectGuess() {
+  console.log('compare logic');
+  if (secretNumber == guess) {
+    console.log('Correct!');
+    document.body.style.backgroundColor = 'green';
+  } else {
+    console.log('Incorrect');
+    document.querySelector('.score').textContent--;
+    if (document.querySelector('.score').textContent == 0) {
+      console.log('Game Over');
+      document.body.style.backgroundColor = 'red';
+    }
+  }
+}
+
+function resetGame() {
+  console.log('Reset Game');
+  document.body.style.backgroundColor = 'black';
+  document.querySelector('.guess').value = 0;
+  document.querySelector('.score').textContent = 20;
+  return getRandomInt(1, 20);
+}
+
+//gen a rand num on doc load
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Document loaded');
+  //secretNumber = getRandomInt(1, 20);
+  document.querySelector('.guess').value = 0;
+  console.log(secretNumber);
+});
+
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+  guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
-  if (!guess) {
-    document.querySelector('.message').textContent = '🎮 Enter a number!';
+  function checkGuess() {
+    console.log('Checking Guess...');
+    if (guess > 0 && guess < 21) {
+      console.log('Range is correct');
+      isCorrectGuess();
+    } else {
+      console.log('Incorrect Range');
+    }
   }
+  //some logic to ensure a number was entered
+  if (!guess) {
+    console.log(typeof guess);
+    document.querySelector('.message').textContent = 'Enter a number!';
+  } else {
+    document.querySelector('.message').textContent = 'Good Guess!';
+    checkGuess();
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = resetGame();
+  console.log(secretNumber);
 });
